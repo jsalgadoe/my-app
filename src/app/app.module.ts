f5b4a76da282045b20fd722e941d85ca1e4525d9
jorge salgado echeverria <jsalgadoecheverria@gmail.com>
2019-09-12 17:15:02 -0500

    mi primera app

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
+import { LoginComponent } from './login/login.component';
+import { ThemeComponent } from './theme/theme.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ThemeComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {RouterModule,Routes} from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ThemeComponent } from './theme/theme.component';
+
+
+
+export const routes : Routes =[
+    {path:'login',component:LoginComponent},
+    {path:'pages',component:ThemeComponent,
+    children:[
+        {path:'',
+        loadChildren:'./pages/dashboard/dashboard.module#DashboardModule'}
+    
+    ]}
+]
+
+@NgModule ({
+   imports:[RouterModule.forRoot(routes,{useHash:true})],
+    exports:[RouterModule],providers:[]
+})
+
+export class AppRoutingModule {}
